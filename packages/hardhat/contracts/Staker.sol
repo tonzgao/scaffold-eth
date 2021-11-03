b103ab842a9624523328ba1843c723bf6604abc5
Tony Gao <tonyzgao@gmail.com>
2021-11-03 14:40:23 -0400

    checkpoint 3: initial implementation

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -4,34 +4,54 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
+    }
 
-  constructor(address exampleExternalContractAddress) public {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    uint256 public constant threshold = 1 ether;
+    uint256 public deadline = now + 30 seconds;
 
-  uint256 public constant threshold = 1 ether;
+    bool public openForWithdraw = false;
 
-  mapping (address => uint256) public balances;
+    mapping(address => uint256) public balances;
 
-  event Stake(address _address, uint256 _amount);
+    event Stake(address _address, uint256 _amount);
 
-  function stake() external payable {
-    balances[msg.sender] = balances[msg.sender] + msg.value;
-    emit Stake(msg.sender, msg.value);
-  }
+    function stake() external payable {
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+        emit Stake(msg.sender, msg.value);
+    }
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    // After some `deadline` allow anyone to call an `execute()` function
+    //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    function execute() external {
+        require(now >= deadline);
+        if (balances[msg.sender] >= threshold) {
+            exampleExternalContract.complete{value: address(this).balance}();
+        } else {
+            openForWithdraw = true;
+        }
+    }
 
+    // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    function withdraw() external {
+        require(openForWithdraw);
+        (bool sent, bytes memory data) = msg.sender.call{
+            value: balances[msg.sender]
+        }("");
+        require(sent, "Failed to send Ether");
+        balances[msg.sender] = 0;
+    }
 
-
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
+    // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    function timeLeft() public view returns (uint256) {
+        if (now >= deadline) {
+            return 0;
+        }
+        return deadline - now;
+    }
 }
